5125223c2dac4f3b49a74cd41da600aa57dfd3cc
Thiago Fang <thiagofang@gmail.com>
2023-12-30 12:36:01 -0300

    ✨ feat: creating my own card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import Card from "@/components/Card";
+
 export default function Home() {
 	return (
 		<main>
 			<div>Teste</div>
+
+			<Card>
+				Novo Card
+			</Card>
 		</main>
 	);
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,16 @@
-const Card = () => {
-	return <div>Card</div>;
+import { ComponentProps, ReactNode } from "react";
+import { VariantProps, tv } from "tailwind-variants";
+
+export const CardStyles = tv({
+	base: "bg-white p-4 rounded-xl shadow-lg dark:bg-dark-container dark:shadow-slate-950/10 shadow-slate-200",
+})
+
+export type CardProps = ComponentProps<"div"> & VariantProps<typeof CardStyles> & {
+	children: ReactNode;
+}
+
+const Card = ({ children, className, ...rest }: CardProps) => {
+	return <div className={CardStyles({ className })} {...rest}>{children}</div>;
 };
 
 export default Card;
