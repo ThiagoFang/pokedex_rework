9a3addb67553d4e5923af813c57c3630881a6564
Thiago Fang <thiagofang@gmail.com>
2024-01-27 23:02:09 -0300

    ✨ feat: new link to header

--- src/components/ui/Header/index.tsx
+++ src/components/ui/Header/index.tsx
@@ -15,23 +15,31 @@ export default function Header() {
       <ul className="flex items-center">
         <li>
           <Link href="/" className="flex items-center gap-1 py-2 px-4">
-            <Lucide.Home size={20} className="stroke-gray-700 dark:stroke-white" />
+            <Lucide.Home size={18} className="stroke-gray-700 dark:stroke-white" />
             <Title>Home</Title>
           </Link>
         </li>
 
         <li>
           <Link href="/pokemon" className="flex items-center gap-1 py-2 px-4">
-            <Lucide.PawPrint size={20} className="stroke-gray-700 dark:stroke-white" />
+            <Lucide.PawPrint size={18} className="stroke-gray-700 dark:stroke-white" />
             <Title>Pokedex</Title>
           </Link>
         </li>
 
         <li>
           <Link href="" className="flex items-center gap-1 py-2 px-4">
-            <Lucide.Gamepad2 size={20} className="stroke-gray-700 dark:stroke-white" />
+            <Lucide.Gamepad2 size={18} className="stroke-gray-700 dark:stroke-white" />
             <Title>Play</Title>
           </Link>
+        </li>
+
+
+        <li>
+          <a href="https://pokeapi.co/" target="_blank" rel="noreferrer" className="flex items-center gap-1 py-2 px-4">
+            <Lucide.ExternalLink size={18} className="stroke-gray-700 dark:stroke-white" />
+            <Title>PokeApi</Title>
+          </a>
         </li>
       </ul>
     </nav>
