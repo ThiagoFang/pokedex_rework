8256ae31cba808112a4007df1135af352a7ccbac
Thiago Fang <thiagofang@gmail.com>
2024-03-07 11:00:07 -0300

    metadata

--- src/app/pokemon/play/page.tsx
+++ src/app/pokemon/play/page.tsx
@@ -1,9 +1,14 @@
 import { Suspense } from "react";
 import { GameArea } from "./components/GameArea";
 import { Title, Text } from "@/components";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Play | Pokedex',
+  description: 'A game to identify pokemon',
+}
 
 function Play() {
-
   return (
     <div className="max-w-7xl mt-6 sm:mt-16 mx-auto">
       <div className="text-center pb-4">
